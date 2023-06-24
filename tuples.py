23f63392185691676792c5038247a4378521219d
NiladriPramanik <107825176+NiladriPramanik@users.noreply.github.com>
2023-06-25 02:47:18 +0530

    Update tuples.py

--- tuples.py
+++ tuples.py
@@ -17,3 +17,17 @@ print(len(tup1))
 tup2=(1,'a',2,3)
 tup3=(4,'b',5,6)
 print(tup2+tup3)
+
+#repeating elements of tuple
+tup1=(1,'a',2)
+print(tup1*3)
+
+#repeating and concatenating
+tup1=(1,'a',2)
+tup2=(4,5,6)
+print(tup1*2+tup2)
+
+#minimum and maximum value of tuple
+tup1=(1,3,2)
+print(min(tup1))
+print(max(tup1))
