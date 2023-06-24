42709172276dd2f9f7365947540d05e104eeaf2e
NiladriPramanik <107825176+NiladriPramanik@users.noreply.github.com>
2023-06-24 22:52:52 +0530

    Update operators.py

--- operators.py
+++ operators.py
@@ -1,7 +1,9 @@
 # arithmetic operators
 # + , - , * , / , %
+
 a=10
 b=20
+print("arithmetic operators")
 print(a+b)
 print(a-b)
 print(b-a)
@@ -10,8 +12,31 @@ print(b/a)
 print(a*b)
 print(a%b)
 print(b%a)
+
+#relational operators <,>,==,!=
+
+print("relational operators")
+print(a>b)
+print(a<b)
+print(a==b)
+print(b==a)
+print(a!=b)
+print(b>a)
+print(b<a)
+
+#logical operators 
+
+a=True
+b=False
+print("logical operators")
+print(a&a)
 print(a&b)
+print(b&b)
+print(b&a)
 print(a|b)
-print(~a)
-print(~b)
+print(a|a)
+print(b|b)
+print(b|a)
+
+
 
