7329030f51958f63b98dece63e6b06648a4de0ba
NiladriPramanik <107825176+NiladriPramanik@users.noreply.github.com>
2023-06-25 01:41:12 +0530

    Create tuples.py

---
+++ tuples.py
@@ -0,0 +1,10 @@
+tup1=(1,"a",True,2,"b",False)
+
+#printing elements of tuple
+
+print(tup1)
+
+#extracting elements of tuple
+
+print(tup1[1:4])
+print(tup1[0:6])  #to extract the last element we need to write index of last + 1.
