70c43bc1fb3b6a5451598be4674eb3b5db0481ac
NiladriPramanik <107825176+NiladriPramanik@users.noreply.github.com>
2023-06-25 01:53:11 +0530

    Update tuples.py

--- tuples.py
+++ tuples.py
@@ -1,10 +1,19 @@
 tup1=(1,"a",True,2,"b",False)
 
 #printing elements of tuple
-
 print(tup1)
 
 #extracting elements of tuple
 
 print(tup1[1:4])
-print(tup1[0:6])  #to extract the last element we need to write index of last + 1.
+
+#to extract the last element we need to write index of last + 1.
+print(tup1[0:6])  
+
+#finding length of tuple
+print(len(tup1))
+
+#concatenating tuples
+tup2=(1,'a',2,3)
+tup3=(4,'b',5,6)
+print(tup2+tup3)
